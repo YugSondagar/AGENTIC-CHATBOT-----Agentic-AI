31d6e24fa5c7f6aed9134546cdb7c334ab8eef13
Yug Sondagar <yug.sondagar11@gmail.com>
2025-12-09 23:00:36 +0530

    Chatbot With Tools completed

--- src/langgraphagenticai/graph/graph_builder.py
+++ src/langgraphagenticai/graph/graph_builder.py
@@ -1,6 +1,9 @@
 from langgraph.graph import StateGraph,START,END
 from src.langgraphagenticai.state.state import State
 from src.langgraphagenticai.nodes.basic_chatbot_node import BasicChatbotNode
+from src.langgraphagenticai.tools.search_tool import create_tool_node,get_tools
+from langgraph.prebuilt import tools_condition,ToolNode
+from src.langgraphagenticai.nodes.chatbot_with_tool_node import ChatbotWithToolNode
 
 class GraphBuilder:
     def __init__(self,model):
@@ -18,6 +21,32 @@ class GraphBuilder:
         self.graph_builder.add_node("chatbot",self.basic_chatbot_node.process)
         self.graph_builder.add_edge(START,"chatbot")
         self.graph_builder.add_edge("chatbot",END)
+
+    def chatbot_with_tools_build_graph(self):
+        """
+        Builds an advanced chatbot graph with tool integration.
+        This method creates a chatbot graph that includes both the chatbot and a tool node. It defines tools, initializes the chatbot with tool capabilities, and sets up conditional and direct edges between nodes. The chatbot node is set as the entry point.
+        """
+        ##Define the tool and tool node
+        tools = get_tools()
+        tool_node = create_tool_node(tools)
+
+        ##Define the LLM
+        llm = self.llm
+
+        ##Define the chatbot node
+        obj_chatbot_with_nodes = ChatbotWithToolNode(llm)
+        chatbot_node = obj_chatbot_with_nodes.create_chatbot(tools)
+
+        ##Add nodes
+        self.graph_builder.add_node("chatbot",chatbot_node)
+        self.graph_builder.add_node("tools",tool_node)
+
+        self.graph_builder.add_edge(START,"chatbot")
+        self.graph_builder.add_conditional_edges("chatbot",tools_condition)
+        self.graph_builder.add_edge("tools","chatbot")
+        self.graph_builder.add_edge("chatbot",END)
+
     
     def setup_graph(self,usecase:str):
         """
@@ -25,5 +54,8 @@ class GraphBuilder:
         """
         if usecase == "Basic Chatbot":
             self.basic_chatbot_build_graph()
+        
+        if usecase == "Chatbot with Web":
+            self.chatbot_with_tools_build_graph()
 
         return self.graph_builder.compile()
--- src/langgraphagenticai/ui/streamlitui/display_result.py
+++ src/langgraphagenticai/ui/streamlitui/display_result.py
@@ -25,3 +25,19 @@ class DisplayResultStreamlit:
                         st.write(user_message)
                     with st.chat_message("assistant"):
                         st.write(value["messages"][0].content)
+        elif usecase == "Chatbot with Web":
+            #Prepare state and invoke the graph
+            initial_state = {'messages':[user_message]}
+            res = graph.invoke(initial_state)
+            for message in res['messages']:
+                if type(message) == HumanMessage:
+                    with st.chat_message("user"):
+                        st.write(message.content)
+                elif type(message) == ToolMessage:
+                    with st.chat_message("ai"):
+                        st.write("Tool Call Started")
+                        st.write(message.content)
+                        st.write("Toll Call Ended")
+                elif type(message) == AIMessage and message.content:
+                    with st.chat_message("assistant"):
+                        st.write(message.content)
